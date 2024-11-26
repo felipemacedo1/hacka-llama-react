dc169182b00e0d09aa39d6c582b289f6cbb53a24
Felipe.Macedo <felipealexandrej@gmail.com>
2024-11-26 13:54:51 -0300

    feat(ui): add LoadingSpinner component to improve UX

---
+++ src/components/spinner/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+const LoadingSpinner: React.FC = () => (
+    <div className="flex justify-center items-center h-screen">
+      <div className="spinner">
+        {/* Aqui pode ser um spinner de CSS ou um componente de biblioteca */}
+        Carregando...
+      </div>
+    </div>
+  );
+  
+  export default LoadingSpinner;
+  
