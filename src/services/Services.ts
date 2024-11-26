1215f49efbe56e17b4e2285309fd2fee1f77f775
Felipe.Macedo <felipealexandrej@gmail.com>
2024-11-26 14:58:35 -0300

    refactor(api): standardize function names and parameters in English

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,24 +1,45 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://lojagamesy.onrender.com/'
-})
+  baseURL: 'https://lojagamesy.onrender.com/', // API Base URL
+});
 
-export const buscar = async(url: string, setDados: Function) => {
-  const resposta = await api.get(url )
-  setDados(resposta.data)
-}
+/**
+ * Fetches data from a given URL and sets it to the provided state function.
+ * @param url - API endpoint
+ * @param setData - State setter function
+ */
+export const fetchData = async (url: string, setData: Function) => {
+  const response = await api.get(url);
+  setData(response.data);
+};
 
-export const cadastrar = async(url: string, dados: Object, setDados: Function) => {
-  const resposta = await api.post(url, dados )
-  setDados(resposta.data)
-}
+/**
+ * Sends a POST request to a given URL with the provided data and sets the response.
+ * @param url - API endpoint
+ * @param data - Data to be sent
+ * @param setData - State setter function
+ */
+export const createData = async (url: string, data: Object, setData: Function) => {
+  const response = await api.post(url, data);
+  setData(response.data);
+};
 
-export const atualizar = async(url: string, dados: Object, setDados: Function) => {
-  const resposta = await api.put(url, dados )
-  setDados(resposta.data)
-}
+/**
+ * Sends a PUT request to update data at a given URL and sets the response.
+ * @param url - API endpoint
+ * @param data - Data to be updated
+ * @param setData - State setter function
+ */
+export const updateData = async (url: string, data: Object, setData: Function) => {
+  const response = await api.put(url, data);
+  setData(response.data);
+};
 
-export const deletar = async(url: string) => {
-  await api.delete(url)
-}
+/**
+ * Sends a DELETE request to a given URL.
+ * @param url - API endpoint
+ */
+export const deleteData = async (url: string) => {
+  await api.delete(url);
+};
